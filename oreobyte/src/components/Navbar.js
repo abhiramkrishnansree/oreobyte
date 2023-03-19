52268fe3cbcdeea6cf8f55d8a1fbdcc13e40584d
abhiramkrishnansree <abhiramkrishnanrajan@gmail.com>
2023-03-19 11:22:08 +0530

    new ever

--- oreobyte/src/components/Navbar.js
+++ oreobyte/src/components/Navbar.js
@@ -8,10 +8,10 @@ const Navbar = () => {
     <div className='navbar-container'>
      <Link to="/kuthara" className='nav-logo'><img src="Logo.png"/></Link>
      <div className='navbar-menu'>
-     <Link to="/kuthara" className='a' style={{color:"white",fontSize:"18px",textDecoration:"none"}}>Home</Link>
-     <Link to="/kuthara" className='a' style={{color:"White",fontSize:"18px",textDecoration:"none"}}>Club</Link>
-     <Link to="/kuthara" className='a' style={{color:"white",fontSize:"18px",textDecoration:"none"}}>Services</Link>
-     <Link to="/kuthara" className='a' style={{color:"white",fontSize:"18px",textDecoration:"none"}}>Contact</Link>
+     <li className='a'><a href="#"  style={{fontSize:"18px",textDecoration:"none"}}>Home</a></li>
+     <li className='a'><a href="#" style={{fontSize:"18px",textDecoration:"none"}}>Service</a></li>
+     <li className='a'><a href="#" style={{fontSize:"18px",textDecoration:"none"}}>About</a></li>
+     <li className='a'><a href="#" style={{fontSize:"18px",textDecoration:"none"}}>Products</a></li>
      </div>
     </div>
     </>
